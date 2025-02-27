f98bbfd94f59685023728fa3576c110195f94394
AndresAAV <andres.artaloytia@gmail.com>
2025-02-27 13:48:02 +0100

    Primera iteracion

--- nth-prime/nth_prime.cpp
+++ nth-prime/nth_prime.cpp
@@ -1,7 +1,50 @@
 #include "nth_prime.h"
 
 namespace nth_prime {
+    
 
-// TODO: add your solution here
+
+    bool is_prime(long int n){
+        if(n==0){
+            throw std::domain_error("Error: 0 is not consider prime");
+
+        }else{
+            for(int i{2}; i<=sqrt(n); i ++){ 
+                if(n % i == 0){
+                    return false; // No es primo
+                }
+            }
+        }
+
+        return true;
+    }
+
+    long int nth(long int n){
+        
+        int aux{2};
+        int number_count{0};
+        int last_prime_numb{0};
+
+        if(n <= 0){
+            throw std::domain_error("No existe un numero primo numero 0");
+        }
+
+        while(number_count != n){
+            
+            while(true){
+                if(is_prime(aux)){
+                    last_prime_numb = aux;
+                    aux++;
+                    break;
+                }
+                aux++;
+            }
+
+            number_count ++;
+        }
+
+        return last_prime_numb;
+
+    }
 
 }  // namespace nth_prime
--- nth-prime/nth_prime.h
+++ nth-prime/nth_prime.h
@@ -1,9 +1,13 @@
 #if !defined(NTH_PRIME_H)
 #define NTH_PRIME_H
 
+#include <stdexcept>
+#include <cmath>
+
 namespace nth_prime {
 
-// TODO: add your solution here
+    long int nth(long int n);
+    bool is_prime(long int n);
 
 }  // namespace nth_prime
 
